c05fe9a3b32ec7cd3a1fa375f34b1d70164a0942
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-11-01 23:21:58 +0100

    ngbootstrap installed
    NavBar edited
    Dropdown created with ngbDropdown

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -29,20 +30,25 @@ import { LoginComponent } from './login/login.component';
     OrderSuccessComponent,
     MyOrdersComponent,
     AdminProductsComponent,
-    AdminOrdersComponent
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
+    NgbModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
       { path: 'products', component: ProductsComponent},
       { path: 'shoppingCart', component: ShoppingCartComponent },
       { path: 'checkOut', component: CheckOutComponent },
       { path: 'orderSuccess', component: OrderSuccessComponent },
-      { path: 'login', component: LoginComponent }
+      { path: 'login', component: LoginComponent },
+      { path: 'admin/products', component: AdminProductsComponent},
+      { path: 'admin/orders', component: AdminOrdersComponent},
+      { path: 'my/orders', component: MyOrdersComponent }
     ])
   ],
   providers: [],
