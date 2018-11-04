6f9e12d5c6539f1ec02971fac89ba795a97a3744
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-11-04 12:08:47 +0100

    login component view
    Non responsive view ready

--- ecommerce/src/app/navbar/navbar.component.ts
+++ ecommerce/src/app/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private afAuth: AngularFireAuth) {
+    afAuth.authState.subscribe(x => console.log(x));
+   }
+  logout() {
+    this.afAuth.auth.signOut();
   }
-
 }
