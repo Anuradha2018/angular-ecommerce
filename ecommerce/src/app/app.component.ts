b7ed60b3f83d8bf862de5abe963b1462620b1797
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-11-05 15:41:57 +0100

    redirecting after loggin implementation added

--- ecommerce/src/app/app.component.ts
+++ ecommerce/src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { AuthService } from './auth.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ecommerce';
+  constructor(private auth: AuthService, router: Router) {
+    auth.user$.subscribe(user => {
+      if (user) {
+        const returnUrl = localStorage.getItem('returnUrl');
+        router.navigateByUrl(returnUrl);
+      }
+    });
+
+  }
 }
