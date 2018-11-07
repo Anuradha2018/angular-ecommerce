956219636b4d778fb8fe78a10172102cc9596bd1
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-11-08 00:18:43 +0100

    installed core.js

--- ecommerce/src/app/auth-guard.service.ts
+++ ecommerce/src/app/auth-guard.service.ts
@@ -2,7 +2,7 @@ import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import 'rxjs/add/operator/map';
-import { map } from 'rxjs/operators';
+
 
 import { Observable } from 'rxjs';
 
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
 export class AuthGuardService implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) { }
-  canActivate(route, state: RouterStateSnapshot) {
+  canActivate(route, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     return this.auth.user$.pipe(map(user => {
       if (user) {
         return true;
--- ecommerce/src/app/auth.service.ts
+++ ecommerce/src/app/auth.service.ts
@@ -6,7 +6,6 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/of';
-import { of } from 'rxjs';
 import * as firebase from 'firebase';
 import { switchMap, map } from 'rxjs/operators';
 
@@ -33,6 +32,11 @@ export class AuthService {
 
   get appUser$(): Observable<AppUser> {
     return this.user$
-    .pipe(switchMap(user => this.userService.get(user.uid)));
+    .pipe(switchMap(user => {
+      if (user) {
+        return this.userService.get(user.uid);
+        return Observable.of(null);
+      }
+    }));
   }
 }
