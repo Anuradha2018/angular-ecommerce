07772e08d19873cf937b8cfbad01b87576a91b4b
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-11-07 10:32:51 +0100

    auth service is corrected

--- ecommerce/src/app/auth.service.ts
+++ ecommerce/src/app/auth.service.ts
@@ -1,12 +1,13 @@
+import { switchMap } from 'rxjs/operators';
 import { UserService } from './user.service';
 import { AppUser } from './models/app-user';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, pipe } from 'rxjs';
+import { of } from 'rxjs/index';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/switchMap';
-import { of } from 'rxjs';
 import * as firebase from 'firebase';
+import { isNull } from '@angular/compiler/src/output/output_ast';
 
 @Injectable()
 export class AuthService {
@@ -31,11 +32,11 @@ export class AuthService {
 
   get appUser$(): Observable<AppUser> {
     return this.user$
-      .switchMap(user => {
-        if (user) return this.userService.get(user.uid);
-
+      .pipe(switchMap(user => {
+        if (user) { return this.userService.get(user.uid);
+        }
         return Observable.of(null);
-      });
+      }));
   }
 }
 
