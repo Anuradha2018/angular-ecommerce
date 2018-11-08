dd9bffdd39b90b718174234f03c4ae1b54ba9c07
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-11-08 10:22:01 +0100

    the error was solved

--- ecommerce/src/app/auth.service.ts
+++ ecommerce/src/app/auth.service.ts
@@ -1,28 +1,29 @@
-import { UserService } from './user.service';
-import { AppUser } from './models/app-user';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AngularFireAuth } from 'angularfire2/auth';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/observable/of';
+import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase';
-import { switchMap, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { AppUser } from './models/app-user';
+import { switchMap } from 'rxjs/operators';
+import { UserService } from './user.service';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthService {
-  user$: Observable<firebase.User>;
+  user$:  Observable<firebase.User>;
 
   constructor(
-    private userService: UserService,
     private afAuth: AngularFireAuth,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private userService: UserService) {
     this.user$ = afAuth.authState;
   }
 
   login() {
     const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/';
     localStorage.setItem('returnUrl', returnUrl);
+
     this.afAuth.auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
   }
 
@@ -32,11 +33,12 @@ export class AuthService {
 
   get appUser$(): Observable<AppUser> {
     return this.user$
-    .pipe(switchMap(user => {
-      if (user) {
-        return this.userService.get(user.uid);
-        return Observable.of(null);
-      }
-    }));
+      .pipe(switchMap(user => {
+        if (user) {
+          return this.userService.get(user.uid).valueChanges();
+        }
+        return of(null);
+      }));
   }
+
 }
