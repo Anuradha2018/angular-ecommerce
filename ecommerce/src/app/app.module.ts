4f79d71985071203827aa19c3d66bbccb362ddfa
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-12-05 20:58:37 +0100

    validation added

--- ecommerce/src/app/app.module.ts
+++ ecommerce/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { CustomFormsModule } from 'ng2-validation';
 
 
 import { AppComponent } from './app.component';
@@ -47,6 +48,7 @@ import { ProductService } from './product.service';
   imports: [
     BrowserModule,
     FormsModule,
+    CustomFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
